a66d140846811a240baf794601d73d8afeff5678
Marc Caramello <marc.caramello@gmail.com>
2024-01-16 23:37:45 -0500

    first commit

---
+++ main.c
@@ -0,0 +1,49 @@
+#include <ncurses/ncurses.h>
+#include <stdio.h>
+#include <time.h>
+#include <windows.h>
+
+/*
+void printRunway() {
+    printw(" -                                      \n");
+    printw("   -                                    \n");
+    printw("     - ________________________________ \n");
+    printw("      |-                               |\n");
+    printw("      |  -                             |\n");
+    printw("      |    -                           |\n");
+    printw("      |      --------------------------|\n");
+    printw("      |                                |\n");
+    printw("      |                                |\n");
+    printw("      |________________________________|\n");
+}
+*/
+
+void printRunway() {
+    printw("                                        \n");
+    printw("                                        \n");
+    printw("       ________________________________ \n");
+    printw("      |                                |\n");
+    printw("      |                                |\n");
+    printw("      |                                |\n");
+    printw("      |                                |\n");
+    printw("      |                                |\n");
+    printw("      |                                |\n");
+    printw("      |________________________________|\n");
+}
+
+int main() {
+    srand(time(0));
+
+    initscr();
+    curs_set(0);
+    noecho();
+    cbreak();
+    keypad(stdscr, true);
+    
+    printRunway();
+    getch();
+    clear();
+
+    endwin();
+    return 0;    
+}
